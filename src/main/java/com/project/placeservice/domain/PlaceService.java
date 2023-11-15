b0a14c48286226f9cc3784bfb15dc53fbc298e6c
João Filipe <joaofilipeb045@gmail.com>
2023-11-15 12:38:13 -0300

    Generate slug

--- src/main/java/com/project/placeservice/domain/PlaceService.java
+++ src/main/java/com/project/placeservice/domain/PlaceService.java
@@ -1,18 +1,21 @@
 package com.project.placeservice.domain;
 
+import com.github.slugify.Slugify;
 import com.project.placeservice.api.PlaceRequest;
 
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest) {
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(), placeRequest.state(),
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()), placeRequest.state(),
                 placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
