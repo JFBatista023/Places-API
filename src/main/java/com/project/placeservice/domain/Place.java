3008d28a841aea4487efba79176e22b98b34b0e8
João Filipe <joaofilipeb045@gmail.com>
2023-11-15 12:16:57 -0300

    Initializing DB and adjustments to entity and controller

--- src/main/java/com/project/placeservice/domain/Place.java
+++ src/main/java/com/project/placeservice/domain/Place.java
@@ -3,7 +3,7 @@ package com.project.placeservice.domain;
 import java.time.LocalDateTime;
 
 public record Place(
-                Long id, String name, String slug, String state,
-                LocalDateTime created, LocalDateTime updatedAt) {
+        Long id, String name, String slug, String state,
+        LocalDateTime createdAt, LocalDateTime updatedAt) {
 
 }
